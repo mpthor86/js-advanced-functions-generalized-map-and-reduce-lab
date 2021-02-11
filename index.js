e42368271840822163bf97ce9a4adda30d69b161
michael <mpthornton86@gmail.com>
2021-02-11 14:19:25 -0700

    commit tree b206bf1e79e3963c238687e85b79137f345fded1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+function map(arr, func) {
+    let newArr = []
+    arr.forEach(el => newArr.push(func(el)))
+    return newArr
+}
+
+function reduce(arr, func, start = 0) {
+    let int = (!!start) ? start : arr[0]
+    let i = (!!start) ? 0 : 1
+    for(; i < arr.length; i++){
+        int = func(arr[i], int)
+    }
+    return int
+}
